d5a2c7ae87808c1ea2ea3175fa7c3f786b6b9c2d
Niki Manoledaki <nikimanoledaki@Guillems-MacBook.local>
2020-03-17 15:39:50 +0000

    Initial commit

---
+++ spec/SolutionANDSpec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Combination where n=1', function () {
+    it('should pass', function () {
+        expect(solution("1")).toBe("1")
+    })
+})
---
+++ src/SolutionAND.js
@@ -0,0 +1,17 @@
+function solution (input) {
+  if (input.length <= 1) return input;
+  var decimals = input.split("")
+
+  return decimals
+}
+
+// function mergeSort (unsortedArray) {
+//   const middle = Math.floor(unsortedArray.length / 2)
+
+//   const left = unsortedArray.slice(0, middle)
+//   const right = unsortedArray.slice(middle)
+
+//   return mergeSort()
+// }
+
+solution("23")
